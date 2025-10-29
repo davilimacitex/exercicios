1f6bf87aa9ec30c6c05517c12fc4972d4540aee6
davilimacitex <davilimacitex@gmail.com>
2025-10-29 00:34:35 +0000

    exercicio

---
+++ exercicos/src/Exercicio1/Notas.java
@@ -0,0 +1,13 @@
+import java.util.Arraylist;
+import java.util.Scanner;
+public class NotasAlunos {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> notas = notas = newArrayList<>();
+    System.out.print("Quantos alunos?" );
+    int qtd = sc.nextInt();
+    for (int i = 0; i< qtd; i++) {
+        System.out.print("Nota do aluno" +(i + 1)" : " );
+    }
+    }
+}
